0c1b579792e0b5dbcefce156916fead85a17c4a9
YC Chen <chency1216@gmail.com>
2019-07-22 19:05:06 +0800

    計算留言長度

--- read.py
+++ read.py
@@ -1,11 +1,12 @@
 data=[]
 count = 0
+message = 0
 with open('reviews.txt', 'r') as f :
 	for line in f :
 		data.append(line)
 		count += 1 
+		message = message + len(line)
 		if count % 1000 == 0:
 			print(len(data)) # 每讀1000筆顯示一次
-print(data[0])
-print('---------------')
-print(data[1])
+print('檔案讀取完畢,總共有', len(data), '筆資料')
+print('留言平均長度為', message/len(data))
